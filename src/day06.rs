e51bc7e1680898b77dc747266943cb01248c6502
Ashlin Eldridge <363071+ashlineldridge@users.noreply.github.com>
2023-03-04 09:59:00 +1000

    Day 6

--- src/day06.rs
+++ src/day06.rs
@@ -1,9 +1,25 @@
-use anyhow::Result;
+use std::collections::HashSet;
 
-pub fn part1(_input: &str) -> Result<String> {
-    Ok("TODO".to_owned())
+use anyhow::{Result, bail};
+
+pub fn part1(input: &str) -> Result<String> {
+    let start = detect_start_marker(input, 4)?;
+    Ok(start.to_string())
 }
 
-pub fn part2(_input: &str) -> Result<String> {
-    Ok("TODO".to_owned())
+pub fn part2(input: &str) -> Result<String> {
+    let start = detect_start_marker(input, 14)?;
+    Ok(start.to_string())
+}
+
+fn detect_start_marker(input: &str, width: usize) -> Result<usize> {
+    let buf = input.as_bytes();
+    for (i, win) in buf.windows(width).enumerate() {
+        let set = win.iter().collect::<HashSet<_>>();
+        if set.len() == width {
+            return Ok(i + width)
+        }
+    }
+
+    bail!("No unique {} character sequence found", width);
 }
